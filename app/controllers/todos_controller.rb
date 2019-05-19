cbb159f7339f1722b2fa6b1a0a6e13e56b21d8dd
Omar <omarelias1997@gmail.com>
2019-05-19 02:16:58 -0400

    adding files

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,56 @@
+class TodosController < ApplicationController
+
+  def index
+    @todos = Todo.all
+  end
+
+  def new
+    @todo = Todo.new
+  end
+
+  def create
+    @todo = Todo.create(todo_params)
+    if @todo.save
+      redirect_to todos_path
+    end
+  end
+
+  def show
+    @todo = Todo.find(params[:id])
+  end
+
+  def edit
+    @todo = Todo.find(params[:id])
+  end
+
+  def update
+    @todo = Todo.find(params[:id])
+    if @todo.update(todo_params)
+      redirect_to todos_path
+    end
+
+  end
+
+  def destroy
+    @todo = Todo.find(params[:id])
+    if @todo.destroy
+      redirect_to todos_path
+    end
+  end
+
+  def complete
+    @todo = Todo.find(params[:id])
+    @todo.update(completed: true)
+    redirect_to todos_path
+  end
+
+  def list
+    @todos = Todo.all
+  end
+
+
+  private
+  def todo_params
+    params.require(:todo).permit(:description, :completed)
+  end
+end
